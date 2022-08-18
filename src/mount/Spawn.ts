cb5b41c73cc35a2e4f805c37b16e76364dd21f46
kmq116 <coderco@foxmail.com>
2022-08-19 00:13:27 +0800

    添加最小孵化条件

--- src/mount/Spawn.ts
+++ src/mount/Spawn.ts
@@ -1,6 +1,7 @@
 import { ROLE } from "role/utils";
 import { creepConfig } from "role/roleConfig";
-
+import { MAIN_ROOM } from "sources/sources";
+const needMinBody = [ROLE.harvester, ROLE.carrier, ROLE.upgrader];
 export class SpawnExtension extends StructureSpawn {
   public spawn(options: {
     body: BodyPartConstant[];
@@ -15,11 +16,18 @@ export class SpawnExtension extends StructureSpawn {
     };
   }): number | undefined {
     if (this.spawning) {
+      console.log(this.spawning, "正在孵化中 spawning");
+
       // return;
     }
     const spawnResult = this.spawnCreep(options.body, options.name, options.opt);
+    console.log("孵化结果", spawnResult);
+    const existEnergy = Game.rooms[MAIN_ROOM].energyAvailable;
+    const energyCapacity = Game.rooms[MAIN_ROOM].energyCapacityAvailable;
+    console.log("已有能量", existEnergy);
+    console.log("能量上限", energyCapacity);
 
-    if (spawnResult === ERR_NOT_ENOUGH_ENERGY) {
+    if (spawnResult === ERR_NOT_ENOUGH_ENERGY && needMinBody.includes(options.opt.memory.role)) {
       const result = this.spawnCreep(creepConfig[options.opt.memory.role].minBody, options.name, {
         memory: {
           role: options.opt.memory.role,
@@ -28,6 +36,7 @@ export class SpawnExtension extends StructureSpawn {
           sourceId: options.opt.memory.sourceId
         }
       });
+      console.log("太大的孵化不出，尝试小一点孵化", result);
 
       return result;
     }
